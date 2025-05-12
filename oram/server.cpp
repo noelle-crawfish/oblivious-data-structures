1934d6d2782062018ffee57279437bf1c9a87e63
Noelle Crawford <noellecrawfish@gmail.com>
2025-05-12 09:24:40 -0500

    re-connection not working

--- oram/server.cpp
+++ oram/server.cpp
@@ -1,12 +1,20 @@
 
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "new_oram.h"
 
 int main() {
-  ORAMServer server = ORAMServer(8080);
+  unsigned int bucket_size = 8;
+  for(unsigned int levels = 2; levels < 4; ++levels) {
+    std::cout << "Re-connecting\n";
+    ORAMServer server = ORAMServer(8080, levels, bucket_size);
 
-  server.run();
+    server.run();
+
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
 
   return 0;
 }
