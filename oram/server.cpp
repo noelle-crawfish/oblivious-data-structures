9e310c2be64547f6d68f2448adcbe308a6a8155f
Vinay Patel <vinay@DESKTOP-AQEIJGG>
2025-05-12 14:19:51 -0500

    ahhh

--- oram/server.cpp
+++ oram/server.cpp
@@ -7,11 +7,12 @@
 #include "new_oram.h"
 
 int main() {
+  int port = 8080; 
   for(auto level_it = levels.begin(); level_it != levels.end(); ++level_it) {
     for(auto bucket_size_it = bucket_sizes.begin(); bucket_size_it != bucket_sizes.end(); ++bucket_size_it) {
       for(auto thresh_it = stash_thresholds.begin(); thresh_it != stash_thresholds.end(); ++thresh_it) {
 	// stash_threshold doesn't matter to server, it's just a repetition
-	ORAMServer server = ORAMServer(8080, *level_it, *bucket_size_it); 
+	ORAMServer server = ORAMServer(port++, *level_it, *bucket_size_it); 
 	server.run();
 	std::this_thread::sleep_for(std::chrono::seconds(1)); // client likes to connect first idk why
       }
